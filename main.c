71230496d6eefad1578e9f58d30cfb05308ca399
khushi2018 <75610698+khushi2018@users.noreply.github.com>
2021-02-12 14:40:32 +0530

    Factorial
    Write a C program to find factorial of a given number.

--- main.c
+++ main.c
@@ -10,35 +10,15 @@ Code, Compile, Run and Debug online from anywhere in world.
 
 void main()
 {
-    int no;   
- 
-     printf("\n Enter Day no between 1-7 :\n ");
-     scanf("%d",&no);
-     switch(no)
-     {
-      case 1:
-       printf("\n Sunday");
-       break;
-      case 2:
-       printf("\n Monday");
-       break;
-      case 3:
-       printf("\n Tuesday");
-       break;
-      case 4:
-       printf("\n Wednesday");
-       break;
-      case 5:
-       printf("\n Thursday");
-       break;
-      case 6:
-       printf("\n Friday");
-       break;
-      case 7:
-       printf("\n Saturday");
-       break;
-      default:
-       printf("\n Please Enter Proper Input");
-     }  
+    int i,no,f=1;
+    printf("Enter no to find out factorial:\n");
+    scanf("%d",&no);
+    i=1;
+    while(i<=no)
+    {
+        f=f*i;
+        i++;
+    }
+    printf("factorial=%d",f);
 }
 
