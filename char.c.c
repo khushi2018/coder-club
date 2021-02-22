5e5d981376d8f93842426d6d27b9228dccc22a54
khushi2018 <75610698+khushi2018@users.noreply.github.com>
2021-02-22 18:50:00 +0530

    changes

--- char.c.c
+++ char.c.c
@@ -1,11 +1,4 @@
-/******************************************************************************
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 /*
 10. Write a C program to check whether the entered character is capital, small letter, digit or any special character.
 */
