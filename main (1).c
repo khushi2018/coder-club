c7ad6a1a37e3f15865e68f635b26d57fa06b4a5d
khushi2018 <75610698+khushi2018@users.noreply.github.com>
2021-02-22 10:30:08 +0530

    sorting

---
+++ main (1).c
@@ -0,0 +1,38 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+void main()
+{
+    int n,a[5],i,j,temp;
+    printf("Enter no.:\n");
+    for(i=0;i<5;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<5;i++)
+    {
+        for(j=i+1;j<5;j++)
+        {
+            if(a[i]>a[j])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    for(i=0;i<=4;i++)
+    {
+        printf("%d\t",a[i]);
+    }
+}
+
+
+
